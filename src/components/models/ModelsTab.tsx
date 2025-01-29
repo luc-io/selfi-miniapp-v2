49aa753d89f53a03bccadf0505ca7742acaad6cd
lvc_io <91891353+luc-io@users.noreply.github.com>
2025-01-28 23:17:49 -0300

    Add placeholder models tab

---
+++ src/components/models/ModelsTab.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Card } from '../ui/Layout';
+
+export function ModelsTab() {
+  return (
+    <Card>
+      {/* TODO: Implement models list UI */}
+      <div className="text-center py-8 text-gray-500">
+        My models will appear here...
+      </div>
+    </Card>
+  );
+}
