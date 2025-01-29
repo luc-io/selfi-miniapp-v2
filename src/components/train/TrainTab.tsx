811dea3391ecae42cba0e8797fa183c91c164509
lvc_io <91891353+luc-io@users.noreply.github.com>
2025-01-28 23:17:42 -0300

    Add placeholder training tab

---
+++ src/components/train/TrainTab.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Card } from '../ui/Layout';
+
+export function TrainTab() {
+  return (
+    <Card>
+      {/* TODO: Implement training UI */}
+      <div className="text-center py-8 text-gray-500">
+        Training feature coming soon...
+      </div>
+    </Card>
+  );
+}
