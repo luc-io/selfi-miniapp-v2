885d2ee59a8452e8ed785872597f6d7ab743a95b
lvc_io <91891353+luc-io@users.noreply.github.com>
2025-01-31 18:04:06 -0300

    Fix TrainTab imports

--- src/components/train/TrainTab.tsx
+++ src/components/train/TrainTab.tsx
@@ -1,13 +1,9 @@
-import React from 'react';
-import { Card } from '../ui/Layout';
+import { Card } from '../ui/components';
 
 export function TrainTab() {
   return (
     <Card>
-      {/* TODO: Implement training UI */}
-      <div className="text-center py-8 text-gray-500">
-        Training feature coming soon...
-      </div>
+      <div>Training tab - Coming soon</div>
     </Card>
   );
 }
