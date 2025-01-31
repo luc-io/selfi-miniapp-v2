fcfd5597283ee7391f9b198b4175207ad26e0444
lvc_io <91891353+luc-io@users.noreply.github.com>
2025-01-31 18:48:58 -0300

    fix: export Slider component

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
 import * as SliderPrimitive from "@radix-ui/react-slider"
  
-const Slider = React.forwardRef<
+export const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
 >(({ className, ...props }, ref) => (
