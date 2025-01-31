89fd378f6696bdab8c1ea45a35d69733fe2035af
lvc_io <91891353+luc-io@users.noreply.github.com>
2025-01-31 18:06:22 -0300

    Update Telegram types

--- src/types/telegram.d.ts
+++ src/types/telegram.d.ts
@@ -8,27 +8,27 @@ interface TelegramUser {
 interface TelegramWebApp {
   initData: string;
   initDataUnsafe: {
-    user?: TelegramUser;
+    user: TelegramUser;
     start_param?: string;
   };
   viewportHeight: number;
   viewportStableHeight: number;
+  isExpanded: boolean;
   expand: () => void;
   close: () => void;
   ready: () => void;
   MainButton: {
+    text: string;
+    isVisible: boolean;
+    isActive: boolean;
     show: () => void;
     hide: () => void;
+    enable: () => void;
+    disable: () => void;
     setText: (text: string) => void;
     onClick: (fn: () => void) => void;
     offClick: (fn: () => void) => void;
   };
   showAlert: (message: string) => void;
   showPopup: (params: { title: string; message: string }) => void;
-}
-
-interface Window {
-  Telegram?: {
-    WebApp?: TelegramWebApp;
-  };
 }
