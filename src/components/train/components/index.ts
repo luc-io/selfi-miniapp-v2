e84bac9ba9752b86d338e88731f753627f50d909
lvc_io <91891353+luc-io@users.noreply.github.com>
2025-02-08 09:55:15 -0300

    feat: add components index file

---
+++ src/components/train/components/index.ts
@@ -0,0 +1,5 @@
+export { FileUpload } from './FileUpload';
+export { ImagePreviews } from './ImagePreviews';
+export { TriggerWordInput } from './TriggerWordInput';
+export { TrainingSteps } from './TrainingSteps';
+export { TrainingToggles } from './TrainingToggles';
