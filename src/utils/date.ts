dba2364a4c8f0c180bb6ad379260855d20763bef
lvc_io <91891353+luc-io@users.noreply.github.com>
2025-02-10 07:39:28 -0300

    feat: add date formatter utility

---
+++ src/utils/date.ts
@@ -0,0 +1,9 @@
+export function formatDate(date: Date): string {
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+  }).format(date);
+}
