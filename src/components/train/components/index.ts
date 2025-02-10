f455d4fa95c7cb6b97a5d3225b43a56264662c70
lvc_io <91891353+luc-io@users.noreply.github.com>
2025-02-10 18:44:27 -0300

    fix: export TrainingProgress component

--- src/components/train/components/index.ts
+++ src/components/train/components/index.ts
@@ -3,3 +3,4 @@ export { ImagePreviews } from './ImagePreviews';
 export { TriggerWordInput } from './TriggerWordInput';
 export { TrainingSteps } from './TrainingSteps';
 export { TrainingToggles } from './TrainingToggles';
+export { TrainingProgress } from './TrainingProgress';
