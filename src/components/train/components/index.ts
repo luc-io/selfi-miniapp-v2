28ed397a51137ecee1e08de27662a474cc62fae8
lvc_io <91891353+luc-io@users.noreply.github.com>
2025-02-10 22:03:50 -0300

    fix: add proper exports for all components and hooks

--- src/components/train/components/index.ts
+++ src/components/train/components/index.ts
@@ -2,3 +2,8 @@ export * from './TrainingForm';
 export * from './CostDisplay';
 export * from './ErrorDisplay';
 export * from './TrainingStatus';
+export * from './FileUpload';
+export * from './ImagePreviews';
+export * from './TriggerWordInput';
+export * from './TrainingSteps';
+export * from './TrainingToggles';
